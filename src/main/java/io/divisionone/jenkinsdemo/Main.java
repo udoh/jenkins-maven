540b70498e35e7616033f60eee3e8876e65f911b
Udo Hoppenworth <udo@autorentals.com>
2019-11-02 15:11:08 -0400

    Test build error

--- src/main/java/io/divisionone/jenkinsdemo/Main.java
+++ src/main/java/io/divisionone/jenkinsdemo/Main.java
@@ -3,6 +3,6 @@ package io.divisionone.jenkinsdemo;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        system.out.println("Hello World!");
     }
 }
