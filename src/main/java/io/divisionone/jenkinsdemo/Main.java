e8fb047241928e9366281d1a9f90673fde10c8e8
Udo Hoppenworth <udo@autorentals.com>
2019-11-02 15:11:49 -0400

    Fix build error

--- src/main/java/io/divisionone/jenkinsdemo/Main.java
+++ src/main/java/io/divisionone/jenkinsdemo/Main.java
@@ -3,6 +3,6 @@ package io.divisionone.jenkinsdemo;
 public class Main {
 
     public static void main(String[] args) {
-        system.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
